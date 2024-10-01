fd20f2a4b3ee446bb0491fa95ab45b9b688750ea
miguel Borquez <miguelborquez7@gmail.com>
2024-09-30 17:27:03 -0700

    migration g unique to users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Action mailer devise intruction step 1 required
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
---
+++ db/migrate/20241001002219_add_unique_index_to_users_email.rb
@@ -0,0 +1,4 @@
+class AddUniqueIndexToUsersEmail < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
