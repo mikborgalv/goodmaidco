b349224360f685b0515866956f6ae05a2626b9e5
miguel Borquez <miguelborquez7@gmail.com>
2024-10-12 20:47:46 -0700

    user dashboard implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
-
       # Prevent access to ActiveAdmin if the user is not an admin
   def authenticate_active_admin_user!
     authenticate_user! # Devise's method to authenticate users
     unless current_user.admin?
       flash[:alert] = "You are not authorized to access this page."
-      redirect_to(root_path)
+      redirect_to(new_user_registration_path)
     end
   end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,10 @@
 class Account < ApplicationRecord
     has_many :locations, dependent: :destroy
-  
     validates :name, :phone_number, presence: true
 
     def self.ransackable_associations(auth_object = nil)
       ["locations"]
     end
-
     def self.ransackable_attributes(auth_object = nil)
       ["billing_address", "created_at", "id", "name", "phone_number", "updated_at"]
     end
